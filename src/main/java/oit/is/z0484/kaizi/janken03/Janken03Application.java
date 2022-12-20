9e76604d82951f5ee1fae69fbbba767a6ee1efb3
e1b20038 <e1b20038@oit.ac.jp>
2022-12-20 22:18:28 +0900

    初期化

---
+++ src/main/java/oit/is/z0484/kaizi/janken03/Janken03Application.java
@@ -0,0 +1,13 @@
+package oit.is.z0484.kaizi.janken03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken03Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z0484/kaizi/janken03/Janken03ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z0484.kaizi.janken03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
