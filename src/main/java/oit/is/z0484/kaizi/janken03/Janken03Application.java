05d699c7574d6ac296107b87651d24ce6619b450
e1b20038 <e1b20038@oit.ac.jp>
2022-12-21 12:31:54 +0900

    lec02完了

--- src/main/java/oit/is/z0484/kaizi/janken03/Janken03Application.java
+++ src/main/java/oit/is/z0484/kaizi/janken03/Janken03Application.java
@@ -1,7 +1,8 @@
 package oit.is.z0484.kaizi.janken03;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.
+    SpringBootApplication;
 
 @SpringBootApplication
 public class Janken03Application {
