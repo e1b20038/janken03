9d9d28c106397244c791ed9890d10a8e513bf65e
e1b20038 <e1b20038@oit.ac.jp>
2022-12-21 19:15:14 +0900

    lec03完成

--- src/main/java/oit/is/z0484/kaizi/janken03/controller/Janken03Controller.java
+++ src/main/java/oit/is/z0484/kaizi/janken03/controller/Janken03Controller.java
@@ -1,30 +1,44 @@
 package oit.is.z0484.kaizi.janken03.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.z0484.kaizi.janken03.model.Entry;
 import oit.is.z0484.kaizi.janken03.model.Janken;
 
 @Controller
+@RequestMapping("/janken03")
 public class Janken03Controller {
 
-  @GetMapping("/janken03")
-  public String janken03() {
+  @Autowired
+  private Entry entry;
+
+  @GetMapping("entry")
+  public String janken03(Principal prin,  ModelMap model) {
     System.out.println("GET");
+    String login_name= prin.getName();
+    this.entry.addUser(login_name);
+    model.addAttribute("name", login_name);
+    model.addAttribute("entry", this.entry);
+
     return "janken.html";
   }
 
-  @PostMapping("/janken03")
+  @PostMapping("login")
   public String janken(@RequestParam String name, ModelMap model) {
     System.out.println("POST");
     model.addAttribute("name", name);
     return "janken.html";
   }
 
-  @GetMapping("/hand")
+  @GetMapping("hand")
   public String userHand(@RequestParam String user_hand, ModelMap model) {
     String cpu_hand = Janken.cpuHand();
     String result = Janken.judge(user_hand, cpu_hand);
